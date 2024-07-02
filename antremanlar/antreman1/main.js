dec7447aadf3904a33f573924887d9ace0345307
emre g. <emreodev1998@gmail.com>
2024-07-02 21:48:31 +0300

    antreman temp 1

--- antremanlar/antreman1/main.js
+++ antremanlar/antreman1/main.js
@@ -3,17 +3,14 @@ path = require('path'),
 app = express(),
 staticFiles=require("./controllers/staticController.js"),
 personController=require("./controllers/personController.js"),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+layouts = require("express-ejs-layouts");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  //for using req.body... for reading post data
-  var bodyParser = require('body-parser')
-  const jsonParser = bodyParser.json();
-  var urlencodedParser = bodyParser.urlencoded({ extended: false })
-///
-
-  //app.use(layouts);
-  app.use(express.static(path.join(__dirname, 'public')));
+ //app.use(layouts);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");//önemli
@@ -21,7 +18,7 @@ app.set("view engine", "ejs");//önemli
 
 //MONGOOSE BETTER FOR MVC STRUCTURE(BÜTÜN ELEMANLAR BELLİ BİR CLASS FORMATINDA DÜZEN İCİN)
 mongoose.connect(
-"mongodb://localhost:27017/antreman1"
+"mongodb://localhost:27017/antreman_template"
 );
 const db = mongoose.connection;
 
@@ -30,10 +27,6 @@ db.once("open", () => {
     });
 
 
-
-app.get("/",staticFiles.indexPage);
-app.get("/style.css",staticFiles.cssIndexPage);
- app.post("/newPerson",urlencodedParser,personController.createPerson);
 
     
 app.listen(app.get("port"), () => {
---
+++ antremanlar/antreman_template/main.js
@@ -0,0 +1,4 @@
+const express = require("express"),
+path = require('path'),
+app = express(),
+mongoose = require("mongoose");
